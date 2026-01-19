78c299e131a9b23b57ccf63575933e5fd2ce61cb
Rafael <rafaelmarques0705@gmail.com>
2026-01-19 10:32:54 -0300

    gestor de transportadora

--- gestor final.py
+++ gestor final.py
@@ -1,3 +1,26 @@
+import pandas as pd
+
+# Caminho do seu arquivo (LEMBRE-SE: FECHE O EXCEL ANTES!)
+caminho = r'C:\Users\Rafae\Downloads\Exel\Primeiro exel.xlsx'
+
+try:
+    # 1. Lê a planilha colorida
+    dados = pd.read_excel(caminho)
+
+    # 2. Cria o novo funcionário
+    novo = pd.DataFrame({'Nome:': ['Marcos'], 'Salario:': [4000]})
+
+    # 3. Junta tudo
+    dados_finais = pd.concat([dados, novo], ignore_index=True)
+
+    # 4. Salva de volta
+    # Mude o nome do arquivo final para ter certeza que é um NOVO
+    dados_finais.to_excel(r'C:\Users\Rafae\Downloads\Exel\TESTE_FINAL.xlsx', index=False)
+    print("Procure pelo arquivo TESTE_FINAL na pasta Exel!")
+
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
+
 funcionarios = [
     {"nome": "Rafael", "cargo": "Desenvolvedor Frontend", "salario": 6000.0},
     {"nome": "Ana", "cargo": "Designer UX", "salario": 5200.0},
@@ -42,5 +65,5 @@ while True:
             print(f"Nome: {n['nome']} | Cargo: {n['cargo']} | Salário: R$ {n['salario']:.2f}")
 
     if resposta == "4":
-        print("saindo...")
+        print("saindo...    ")
         exit()
